5b646c23ae95b15c77e11cd965c43e9daefb9d13
@sammyolsson <sammyolsson@gmail.com>
2024-02-09 21:41:08 +0100

    Learning

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674809a50929a3e9a20f35cee712b20c916047ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b756404b1000b51657951361f33689ef8063f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
