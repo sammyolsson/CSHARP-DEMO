195827d304b498de460aec178d0c8dfda505df8c
@sammyolsson <sammyolsson@gmail.com>
2024-02-13 21:07:25 +0100

    Learning

--- Program.cs
+++ Program.cs
@@ -76,6 +76,7 @@ Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
 
 */
 
+/*
 string str = "The quick brown fox jumps over the lazy dog.";
 // convert the message into a char array
 char[] charMessage = str.ToCharArray();
@@ -88,4 +89,58 @@ foreach (char i in charMessage) { if (i == 'o') { x++; } }
 string new_message = new String(charMessage);
 // print it out
 Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+Console.WriteLine($"'o' appears {x} times."); */
+
+
+/*
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+*/
+
+/*
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+*/
+
+/*
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");
+*/
+
+/*
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+*/
+
+/*
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+*/
+
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("cow"));
+
+// int a = 7;
+// int b = 6;
+// Console.WriteLine(a != b);
+// string s1 = "Hello";
+// string s2 = "Hello";
+// Console.WriteLine(s1 != s2);
+
+int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e86987cd331ab196602f95f9fb248ace2826b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c81a28219afa2399ef7bb55dd71c535272643c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
